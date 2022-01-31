1fa03ca31da5d63fa762509e73e901ecac94850f
Abhinav Sharma <abhinav.161a026@gmail.com>
2022-01-31 23:42:38 +0530

    node-3

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const sum= require('./sum')
-console.log(sum(5,6))
+// const sum= require('./sum')
+// console.log(sum(5,6))
+
+const http= require('http')
+
+const server= http.createServer((req,res)=>{
+    res.writeHead(200)
+    res.end(JSON.stringify({
+        data:'Hello World'
+    }))
+})
+
+server.listen(3002,()=>{
+    console.log('listening on port 3002')
+})
